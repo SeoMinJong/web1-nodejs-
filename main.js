4c95972b4b2e29d376439124d217c91ea088dcdb
Seo Min Jong <tjalswhd113@naver.com>
2023-07-17 12:30:00 +0900

    nodejs - readFile&parsing link

--- main.js
+++ main.js
@@ -8,9 +8,6 @@ var app = http.createServer(function(req, res){
 
     var title = query_date.get('id');
 
-    console.log('==============================')
-    console.log('before : '+title)
-
     if(title == null){
         title = 'Welcome'
     };
@@ -20,8 +17,9 @@ var app = http.createServer(function(req, res){
     }
 
     res.writeHead(200);
-
-    var template = `
+    console.log(title)
+    fs.readFile(`data/${title}`, 'utf-8', function(err, description){
+        var template = `
 <!doctype html>
 <html>
 
@@ -39,20 +37,17 @@ var app = http.createServer(function(req, res){
         <li><a href="/?id=JavaScript">JavaScript</a></li>
     </ol>
     <h2>${title}</h2>
-    <p>The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources
-        are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the
-        Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web
-        browser computer program in 1990 while employed at CERN in Switzerland.[2][3] The Web browser was released
-        outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public
-        on the Internet in August 1991.
+    <p>
+        ${description}
     </p>
 </body>
 
 </html>
 `
-
-    console.log('after : '+title)
     res.end(template);
 })
+    })
+
+    
 
 app.listen(3000); 
