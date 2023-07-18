19339f2a1fc8183b1948261c5bc3a2bd8ac7c5ed
Seo Min Jong <tjalswhd113@naver.com>
2023-07-18 19:08:20 +0900

    nodejs - basic array syntax

---
+++ syntax/array.js
@@ -0,0 +1,12 @@
+//CURD
+
+var arr1 = ['A','B','C','D'];
+console.log(arr1[1]);
+console.log(arr1[3]);
+arr1[2] = '3';
+console.log(arr1[2]);
+console.log(arr1.length);
+
+arr1.push('E');
+console.log(arr1.length);
+console.log(arr1);
