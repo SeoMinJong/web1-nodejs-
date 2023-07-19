67e8dbd44b2600abc2d1b482eee03bc39c6adba0
Seo Min Jong <tjalswhd113@naver.com>
2023-07-19 22:03:51 +0900

    nodejs - app function apply

--- main.js
+++ main.js
@@ -2,6 +2,42 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title, list, body){
+
+    return `
+    <!doctype html>
+    <html>
+    
+    <head>
+        <meta charset="utf-8">
+        <link rel="icon" href="data:,">
+        <title>WEB1 - ${title}</title>
+    </head>
+    
+    <body>
+        <h1><a href="/">WEB</a></h1>
+        ${list}
+
+    ${body}
+    </body>
+    
+    </html>
+    `
+};
+
+function templateList(filelist) {
+    _list = '<ul>';
+    var i = 0;
+
+    while (filelist.length > i){
+        _list += `\n<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+        i += 1;
+    };
+    _list += '</ul>'
+
+    return _list
+}
+
 var app = http.createServer(function (req, res) {
     var _url = req.url;
     var query_date = url.parse(_url, true).query;
@@ -17,89 +53,25 @@ var app = http.createServer(function (req, res) {
         if (query_date.id === undefined) {
 
             fs.readdir('./data', function(err, filelist){
-                console.log(filelist)
-
                 var title = 'Welcome';
                 var description = 'Hellom, Node.js';
 
                 // 유동적인 파일 
-                _list = '<ul>';
-                var i = 0;
-
-                while (filelist.length > i){
-                    _list += `\n<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-                    i += 1;
-                    console.log(filelist[i])
-                };
-                _list += '</ul>'
-
-                var template = `
-                    <!doctype html>
-                    <html>
-                    
-                    <head>
-                        <meta charset="utf-8">
-                        <link rel="icon" href="data:,">
-                        <title>WEB1 - ${title}</title>
-                    </head>
-                    
-                    <body>
-                        <h1><a href="/">WEB</a></h1>
-                        ${_list}
-
-                        <h2>${title}</h2>
-                        <p>
-                            ${description}
-                        </p>
-                    </body>
-                    
-                    </html>
-                    `
+                var _list = templateList(filelist);
+                var template = templateHTML(title, _list, `<h2>${title}</h2><p>${description}</p>`);
+                
                 res.writeHead(200);
                 res.end(template);
             })
             
         } else {
             fs.readdir('./data', function(err, filelist){
-                console.log(filelist)
-
-                var title = 'Welcome';
-                var description = 'Hellom, Node.js';
-
                 // 유동적인 파일 목록 리스트
-                _list = '<ul>';
-                var i = 0;
-
-                while (filelist.length > i){
-                    _list += `\n<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-                    i += 1;
-                    console.log(filelist[i])
-                };
-                _list += '</ul>'
+                _list = templateList(filelist);
                 
                 fs.readFile(`data/${query_date.id}`, 'utf-8', function (err, description) {
                     var title = query_date.id;
-                    var template = `
-                    <!doctype html>
-                    <html>
-                    
-                    <head>
-                        <meta charset="utf-8">
-                        <link rel="icon" href="data:,">
-                        <title>WEB1 - ${title}</title>
-                    </head>
-                    
-                    <body>
-                        <h1><a href="/">WEB</a></h1>
-                        ${_list}
-                        <h2>${title}</h2>
-                        <p>
-                            ${description}
-                        </p>
-                    </body>
-                    
-                    </html>
-                    `
+                    var template = templateHTML(title, _list, `<h2>${title}</h2><p>${description}</p>`)
                     res.writeHead(200);
                     res.end(template);
                 })
