4f352df5c8d9ecbdb88bf29bdaaa7f0b65f5ffdb
Seo Min Jong <tjalswhd113@naver.com>
2023-07-21 21:25:06 +0900

    nodejs - post형식 file update

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-function templateHTML(title, list, body){
+function templateHTML(title, list, body, control){
 
     return `
     <!doctype html>
@@ -18,13 +18,16 @@ function templateHTML(title, list, body){
     <body>
         <h1><a href="/">WEB</a></h1>
         ${list}
-        <a href="/create">create</a>
+        ${control}
+        
     ${body}
     </body>
     
     </html>
     `
 };
+
+
 
 function templateList(filelist) {
     _list = '<ul>';
@@ -58,20 +61,23 @@ var app = http.createServer(function (req, res) {
 
                 // 유동적인 파일 
                 var _list = templateList(filelist);
-                var template = templateHTML(title, _list, `<h2>${title}</h2><p>${description}</p>`);
+                var template = templateHTML(title, _list, 
+                    `<h2>${title}</h2><p>${description}</p>`, 
+                    `<a href="/create">create</a>`);
                 
                 res.writeHead(200);
                 res.end(template);
             })
             
         } else {
+            // 유동적인 파일 목록 리스트
             fs.readdir('./data', function(err, filelist){
-                // 유동적인 파일 목록 리스트
-                _list = templateList(filelist);
-                
                 fs.readFile(`data/${query_date.id}`, 'utf-8', function (err, description) {
                     var title = query_date.id;
-                    var template = templateHTML(title, _list, `<h2>${title}</h2><p>${description}</p>`)
+                    var _list = templateList(filelist);
+                    var template = templateHTML(title, _list, 
+                        `<h2>${title}</h2><p>${description}</p>`, 
+                        `<a href="/create">create</a>  <a href="/update?id=${title}">update</a>`);
                     res.writeHead(200);
                     res.end(template);
                 })
@@ -84,7 +90,7 @@ var app = http.createServer(function (req, res) {
             // 유동적인 파일 
             var _list = templateList(filelist);
             var template = templateHTML(title, _list, `
-            <form action="http://localhost:3000/create_process" method="post">
+            <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
                 <textarea name="description" placeholder="description"></textarea>
@@ -92,7 +98,8 @@ var app = http.createServer(function (req, res) {
             <p>
                 <input type="submit">
             </p>
-            </form>`);
+            </form>`,
+            '');
             
             res.writeHead(200);
             res.end(template);
@@ -116,11 +123,60 @@ var app = http.createServer(function (req, res) {
             })
         });
 
-    }else {
+    }else if(pathname === "/update"){
+        // 유동적인 파일 목록 리스트
+        fs.readdir('./data', function(err, filelist){
+            fs.readFile(`data/${query_date.id}`, 'utf-8', function (err, description) {
+                var title = query_date.id;
+                var _list = templateList(filelist);
+                var template = templateHTML(title, _list, 
+                    `
+                    <form action="/update_process" method="post">
+                    <input type="hidden" name="id" value="${title}">
+                    <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                    <p>
+                        <textarea name="description" placeholder="description">${description}</textarea>
+                    </p>
+                    <p>
+                        <input type="submit">
+                    </p>
+                    </form>`, 
+                    `<a href="/create">create</a>  <a href="/update?id=${title}">update</a>`);
+                res.writeHead(200);
+                res.end(template);
+            })
+        })
+    }else if(pathname === "/update_process"){
+        // API post 형식으로 데이터를 받을 때 사용하는 형식
+        var body='';
+        req.on('data', function(data){
+            body = body + data;
+        });
+        req.on('end', function(){
+            var post = qs.parse(body);
+            var id = post.id;
+            var title = post.title;
+            var description = post.description;
+
+            console.log("id : "+id)
+            console.log("title : "+title)
+            console.log("description : "+description)
+
+            fs.rename(`data/${id}`, `data/${title}`, function(err){
+
+            })
+
+            fs.writeFile(`data/${title}`, description, 'utf-8',
+            function(err){
+                res.writeHead(302, {Location: `/?id=${title}`});
+                res.end('success');
+            })
+        })
+    }
+    else {
         res.writeHead(404);
         res.end('Not Found!');
     }
-
 })
 
 
