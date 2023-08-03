025c0d3b9b4b3296e856544b46b2a0b23db14a9b
Seo Min Jong <tjalswhd113@naver.com>
2023-08-03 18:21:25 +0900

    express - basic errorhandler

--- main_ex.js
+++ main_ex.js
@@ -39,26 +39,30 @@ app.get('/', (req, res) => {
     res.send(template);
 })
 
-app.get('/page/:pageId', function(req, res){
-    const filteredID = path.parse(req.params.pageId).base;
-    fs.readFile(`data/${filteredID}`, 'utf-8', function (err, description) {
-        var sanitizedTitle = sanitizeHtml(req.params.pageId);
-        var sanitizedDesscription = sanitizeHtml(description,{
-            allowedTags:['h1']
-        });
-        var _list = template_f.list(req.list);
-        var template = template_f.html(sanitizedTitle, _list, 
-            `<h2>${sanitizedTitle}</h2><p>${sanitizedDesscription}</p>`, 
-            `<a href="/create">create</a>
-            <a href="/update/${sanitizedTitle}">update</a>  
-            <form action="/delete_process" method="post">
-                <input type="hidden" name="id" value="${sanitizedTitle}">
-                <input type="submit" value="delete">
-            </form>`);
-            
-        res.send(template);
-    });
-})
+app.get('/page/:pageId', function(req, res, next){
+        const filteredID = path.parse(req.params.pageId).base;
+        fs.readFile(`data/${filteredID}`, 'utf-8', function (err, description) {
+            if (err){
+                next(err);
+            } else{
+            var sanitizedTitle = sanitizeHtml(req.params.pageId);
+            var sanitizedDesscription = sanitizeHtml(description,{
+                allowedTags:['h1']
+            });
+            var _list = template_f.list(req.list);
+            var template = template_f.html(sanitizedTitle, _list, 
+                `<h2>${sanitizedTitle}</h2><p>${sanitizedDesscription}</p>`, 
+                `<a href="/create">create</a>
+                <a href="/update/${sanitizedTitle}">update</a>  
+                <form action="/delete_process" method="post">
+                    <input type="hidden" name="id" value="${sanitizedTitle}">
+                    <input type="submit" value="delete">
+                </form>`);
+                
+            res.send(template);
+    }})
+    
+});
 
 
 app.get('/create',function(req,res){
@@ -113,27 +117,27 @@ app.post('/create_process', function(req, res){
 });
 
 
-app.get('/update/:pageId', function(req, res){
+app.get('/update/:pageId', function(req, res, next){
     // 유동적인 파일 목록 리스트
-    var filteredID = path.parse(req.params.pageId).base
+    var filteredID = path.parse(req.params.pageId).base;
     fs.readFile(`data/${filteredID}`, 'utf-8', function (err, description) {
-        var title = req.params.pageId;
-        var _list = template_f.list(req.list);
-        var template = template_f.html(title, _list, 
-            `
-            <form action="/update_process" method="post">
-            <input type="hidden" name="id" value="${title}">
-            <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-            <p>
-                <textarea name="description" placeholder="description">${description}</textarea>
-            </p>
-            <p>
-                <input type="submit">
-            </p>
-            </form>`, 
-            `<a href="/create">create</a>  <a href="/update/${title}">update</a>`);
-        res.send(template);
-    })
+            var title = req.params.pageId;
+            var _list = template_f.list(req.list);
+            var template = template_f.html(title, _list, 
+                `
+                <form action="/update_process" method="post">
+                <input type="hidden" name="id" value="${title}">
+                <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                <p>
+                    <textarea name="description" placeholder="description">${description}</textarea>
+                </p>
+                <p>
+                    <input type="submit">
+                </p>
+                </form>`, 
+                `<a href="/create">create</a>  <a href="/update/${title}">update</a>`);
+            res.send(template);
+        });
 })
 
 app.post('/update_process', function(req, res){
@@ -201,6 +205,15 @@ app.post('/delete_process', function(req, res){
         res.end();
     })
 })
+
+app.use(function(req, res, next) {
+    res.status(404).send('Sorry cant find that!');
+});
+
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
 app.listen(port, function(){
     console.log(`Example app listening on port ${port}`)
