48b25903f6e89f9dc754c18a3b804a3a99cc9647
Seo Min Jong <tjalswhd113@naver.com>
2023-08-17 12:51:28 +0900

    nodejs - 파일 정리 및 router 내부 함수 정의 별도 분리

--- main_ex.js
+++ main_ex.js
@@ -1,8 +1,6 @@
 const compression = require('compression');
 const bodyParser = require('body-parser');
 const express = require('express');
-const mysql = require('mysql');
-const fs = require('fs');
 
 const template_f = require('./lib/template.js');
 const topicRouter = require('./router/topic.js');
@@ -15,16 +13,6 @@ const port = 3000
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression())
-
-//use가 아닌 get을 통해 현재 작성된 process 기능의 경우에는 list를 호출하지 않게 할 수 있다.
-//또한 '*'을 작성하여 모든 호출에 대해 리스트를 불러오는 미들웨어가 호출되도록 함
-app.get('*',function(req, res, next){ 
-    fs.readdir('./data', function(err, filelist){
-        req.list = filelist;
-        next();//다음에 호출되어야 할 미들웨어가 실행됨
-    });
-});
-
 
 app.use('/topic', topicRouter);
 
@@ -40,8 +28,6 @@ app.get('/', (req, res) => {
             `<a href="/topic/create">create</a>`);
         res.send(template);
     });
-    
-    
 })
 
 app.use(function(err, req, res, next) {
@@ -57,91 +43,3 @@ app.use(function(err, req, res, next) {
 app.listen(port, function(){
     console.log(`Example app listening on port ${port}`)
 })
-
-
-/*
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-const qs = require('querystring');
-const path = require('path');
-const template_f = require('./lib/template.js');
-const sanitizeHtml = require('sanitize-html');
-
-
-
-var app = http.createServer(function (req, res) {
-    var _url = req.url;
-    var query_date = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-
-    // title만 사용해서 본문 내용을 추가했던 방법
-    // if(title == null){
-    //     title = 'Welcome'
-    // };
-
-    if (pathname === '/') {
-        if (query_date.id === undefined) {
-
-            fs.readdir('./data', function(err, filelist){
-                var title = 'Welcome';
-                var description = 'Hello, Node.js';
-
-                // 유동적인 파일 
-                var _list = template_f.list(filelist);
-                var template = template_f.html(title, _list, 
-                    `<h2>${title}</h2><p>${description}</p>`, 
-                    `<a href="/create">create</a>`);
-                
-                res.writeHead(200);
-                res.end(template);
-            })
-            
-        } else {
-            // 유동적인 파일 목록 리스트
-            fs.readdir('./data', function(err, filelist){
-                var filteredID = path.parse(query_date.id).base;
-                fs.readFile(`data/${filteredID}`, 'utf-8', function (err, description) {
-                    var title = query_date.id;
-                    var sanitizedTitle = sanitizeHtml(title);
-                    var sanitizedDesscription = sanitizeHtml(description,{
-                        allowedTags:['h1']
-                    });
-                    var _list = template_f.list(filelist);
-                    var template = template_f.html(sanitizedTitle, _list, 
-                        `<h2>${sanitizedTitle}</h2><p>${sanitizedDesscription}</p>`, 
-                        `<a href="/create">create</a>
-                        <a href="/update?id=${sanitizedTitle}">update</a>  
-                        <form action="delete_process" method="post">
-                            <input  type="hidden" name="id" value="${sanitizedTitle}">
-                            <input type="submit" value="delete">
-                        </form>`);
-                        
-                    res.writeHead(200);
-                    res.end(template);
-                });
-            });
-        }
-    }else if(pathname === '/create'){})
-    }else if(pathname==='/create_process'){});
-
-    }else if(pathname === "/update"){
-        // 유동적인 파일 목록 리스트
-        
-    }else if(pathname === "/update_process"){
-        // API post 형식으로 데이터를 받을 때 사용하는 형식
-        
-    }else if(pathname === "/delete_process"){
-        // API post 형식으로 데이터를 받을 때 사용하는 형식
-    }
-    else {
-        res.writeHead(404);
-        res.end('Not Found!');
-    }
-})
-
-
-
-app.listen(3000);
-*/
-
--- router/topic.js
+++ router/topic.js
@@ -1,139 +1,31 @@
-const sanitizeHtml = require('sanitize-html');
 const express = require('express');
 const router = express.Router();
-const mysql = require('mysql');
-const path = require('path');
-const fs = require('fs');
 
-const template_f = require('../lib/template.js');
-const db = require('../lib/mysql.js');
-const { time } = require('console');
+const topic_func = require('../lib/topic_func.js');
 
 router.get('/create',function(req,res){
-    db.query('SELECT * FROM topic', function(err, topics){
-        if(err){
-            throw err
-        }
-        var title = 'WEB - CREATE';
-
-        // 유동적인 파일 
-        var _list = template_f.list(topics);
-        var template = template_f.html(title, _list, `
-        <form action="/topic/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-            <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-            <input type="submit">
-        </p>
-        </form>`,
-        '');
-    
-        res.send(template);
-    });
+    topic_func.create_func(req, res);
 });
 
 router.post('/create_process', function(req, res){
-    var post = req.body;
-    console.log("post :",post)
-    db.query(`INSERT INTO topic (title, description, created, author_id) VALUES (?, ?, NOW(), ?)`,[post.title, post.description, 1],
-        function(err, result){
-            if(err){
-                throw err
-            }
-            console.log("result.insertId :",result.insertId)
-            res.writeHead(302, {Location: `/topic/${result.insertId}`});
-            res.end('success');
-    });
-    
+    topic_func.create_process_func(req,res);
 });
 
 router.post('/delete_process', function(req, res){    
-    var post = req.body;
-    var id = post.id;
-    db.query(`DELETE FROM topic WHERE id=?`,[id],
-        function(err, result){
-            if(err){
-                throw err
-            }
-            res.writeHead(302, {Location: `/`})
-            res.end();
-    });
+    topic_func.delete_process_func(req,res);
 });
 
-router.get('/update/:pageId', function(req, res, next){
-    var filteredID = path.parse(req.params.pageId).base;
-    // 유동적인 파일 목록 리스트
-    db.query(`SELECT * FROM topic`, function(err, topics){
-        db.query(`SELECT * FROM topic WHERE id='${filteredID}'`, function(err, target_topic){
-            var title = target_topic[0].title;
-            var _list = template_f.list(topics);
-            var template = template_f.html(title, _list, 
-                `
-                <form action="/topic/update_process" method="post">
-                <input type="hidden" name="id" value="${filteredID}">
-                <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-                <p>
-                    <textarea name="description" placeholder="description">${target_topic[0].description}</textarea>
-                </p>
-                <p>
-                    <input type="submit">
-                </p>
-                </form>`, 
-                `<a href="/topic/create">create</a>  <a href="/topic/update/${title}">update</a>`);
-            res.send(template);
-        })
-    });
+router.get('/update/:pageId', function(req, res){
+    topic_func.update_func(req,res);
 })
 
 router.post('/update_process', function(req, res){
-    var post = req.body;
-    var id = post.id;
-    var title = post.title;
-    var description = post.description;
-    console.log("id :",id)
-    console.log("title :",title)
-    console.log("description :",description)
-
-    db.query(`UPDATE topic SET title=?, description=? WHERE id=?`,[title, description, id], function(err, result){
-        if(err){
-            throw err
-        }
-    })
-    res.writeHead(302, {Location: `/topic/${id}`});
-    res.end();
-
+    topic_func.update_process_func(req,res);
 })
 
 
 router.get('/:pageId', function(req, res, next){
-    const filteredID = req.params.pageId;
-    console.log("filteredID :",filteredID)
-    db.query('SELECT * FROM topic', function(err, topics){
-        if(err){
-            throw err
-        }
-        db.query(`SELECT * FROM topic WHERE id=?`,[filteredID], function(err2, target_topic){
-            if(err2){
-                throw err2
-            }
-
-            var title = target_topic[0].title;
-            var _list = template_f.list(topics);
-
-            var template = template_f.html(title, _list, 
-                `<h2>${title}</h2><p>${target_topic[0].description}</p>`, 
-                `<a href="/topic/create">create</a>
-                <a href="/topic/update/${filteredID}">update</a>  
-                <form action="delete_process" method="post">
-                    <input  type="hidden" name="id" value="${filteredID}">
-                    <input type="submit" value="delete">
-                </form>`);
-                
-            res.send(template);
-        });
-    })
+    topic_func.topic_func(req, res);
 });
 
 module.exports = router;
